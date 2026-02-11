49d7ed92638ef6d2d40c78710cc616f962e11d5a
badgelatte <saucy5939@gmail.com>
2026-02-11 23:44:05 +0900

    feat: 두 수의 최소 공배수 구하기, 해당 값을 이용해 타일 개수 구하기
    두 수의 최소 공배수 구하기, 해당 값을 이용해 타일 개수 구하기

---
+++ src/test/java/com/again/day0211/advanced/MainTest.java
@@ -0,0 +1,30 @@
+package com.again.day0211.advanced;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class MainTest {
+    private static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of(1, 1, 1, 1),
+                Arguments.of(123, 345, 14145, 4715),
+                Arguments.of(2, 81, 162, 162),
+                Arguments.of(3, 7, 21, 21),
+                Arguments.of(19, 87, 1653, 1653),
+                Arguments.of(1, 40, 40, 40)
+        );
+    }
+
+    @ParameterizedTest(name = "{0} and {1}'s LCM is {2}")
+    @MethodSource("testCases")
+    @DisplayName("최소 공배수 테스트")
+    void algorithm_test(int first, int second, int answer1, int answer2) {
+        Assertions.assertEquals(answer1, Main.solution(first, second)[0]);
+        Assertions.assertEquals(answer2, Main.solution(first, second)[1]);
+    }
+}
