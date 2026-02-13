8ef5c1e0794808cdc01df0814e5b22cf8286340e
badgelatte <saucy5939@gmail.com>
2026-02-13 23:07:35 +0900

    feat: factorial, fibonacci 기능 구현 완료
    factorial과 fibonacci를 구현하는 기능 구현

--- src/main/java/com/again/day0213/basic/Main.java
+++ src/main/java/com/again/day0213/basic/Main.java
@@ -3,12 +3,22 @@ package com.again.day0213.basic;
 public class Main {
 
     public static long factorial(int number) {
-        // TODO : factorial
-        return 0;
+        if (number <= 1) {
+            return 1;
+        }
+
+        return number * factorial(number - 1);
     }
 
     public static long fibonacci(int number) {
-        // TODO : fibonacci
-        return 0;
+        if (number <= 1) {
+            return number;
+        }
+
+        if (number == 2) {
+            return 1;
+        }
+
+        return fibonacci(number - 1) + fibonacci(number - 2);
     }
 }
