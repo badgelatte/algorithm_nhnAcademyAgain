8a36a7c013fd8b50170c01ed61f346b6cd1562d4
badgelatte <saucy5939@gmail.com>
2026-02-17 21:22:54 +0900

    feat: 버블 정렬시 오른쪽에서 왼쪽으로 최대로 이동한 거리 구하기 구현
    버블 정렬시 오른쪽에서 왼쪽으로 최대로 이동한 거리 구하기 구현

--- src/main/java/com/again/day0216/basic/Main.java
+++ src/main/java/com/again/day0216/basic/Main.java
@@ -2,7 +2,17 @@ package com.again.day0216.basic;
 
 public class Main {
     public static int solution(int[] unsortedArray, int[] sortedArray) {
+        int maxDistance = 0;
 
-        return 0;
+        for (int i = 0; i < unsortedArray.length - 1; i++) {
+            for(int j = 0; j < sortedArray.length - 1; j++) {
+                if(unsortedArray[i] == sortedArray[j] && maxDistance < i - j) {
+                    maxDistance = i - j;
+                }
+            }
+        }
+
+        return maxDistance;
     }
+
 }
