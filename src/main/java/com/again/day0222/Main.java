96175d802052c73382793ec31124ac3fa0bed42b
badgelatte <saucy5939@gmail.com>
2026-02-22 06:09:51 +0900

    feat: 분할 부분 재귀함수로 코드 수정, 분할 정복 알고리즘을 이용한 병합 정렬 코드 완성
    분할 부분 재귀함수로 코드 수정, 분할 정복 알고리즘을 이용한 병합 정렬 코드 완성

--- src/main/java/com/again/day0222/Main.java
+++ src/main/java/com/again/day0222/Main.java
@@ -1,68 +1,58 @@
 package com.again.day0222;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
     public static String solution(String input) {
-        List<String> list1 = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
+        String[] arr = divideArr(input.split(""));
+        StringBuilder stringBuilder = new StringBuilder();
 
-        list1.add(input.substring(0,input.length()/2));
-        list2.add(input.substring(input.length()/2));
-
-        String left = list1.getFirst();
-        String right = list2.getFirst();
-        int leftSize = 0;
-        int rightSize = 0;
-
-        // divide
-        for (int i = 0; i < input.length()/2 - 1; i++) {
-            leftSize = list1.size();
-            rightSize = list2.size();
-
-            while(leftSize > 0) {
-                left = list1.get(leftSize - 1);
-                leftSize--;
-
-                if(left.length() > 1) {
-                    list1.remove(left);
-                    list1.add(left.substring(0, left.length()/2));
-                    list2.add(left.substring(left.length()/2));
-                }
-
-            }
-
-            while(rightSize > 0) {
-                right = list2.get(rightSize - 1);
-                rightSize--;
-
-                if(right.length() > 1) {
-                    list2.remove(right);
-                    list1.add(right.substring(0, right.length()/2));
-                    list2.add(right.substring(right.length()/2));
-                }
-            }
+        for (String s : arr) {
+            stringBuilder.append(s);
         }
 
-        list1.addAll(list2);
-        int[] arr = strListToIntArr(list1);
-
-
-        String result = "";
-        for (int i = 0; i < arr.length; i++) {
-            result += arr[i];
-        }
-        return result;
+        return stringBuilder.toString();
     }
 
-    public static int[] strListToIntArr(List<String> stringList) {
-        int[] arr = new int[stringList.size()];
-
-        for (int i = 0; i < stringList.size(); i++) {
-            arr[i] = Integer.parseInt(stringList.get(i));
+    public static String[] divideArr(String[] strs) {
+        if (strs.length < 2) {
+            return strs;
         }
 
+        int mid = strs.length/2;
+
+        String[] arr1 = divideArr(Arrays.copyOfRange(strs, 0, mid));
+        String[] arr2 = divideArr(Arrays.copyOfRange(strs,mid, strs.length));
+
+        return mergeArr(arr1, arr2);
+    }
+
+    public static String[] mergeArr(String[] arr1, String[] arr2) {
+        String[] arr = new String[arr1.length + arr2.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+
+            int n1 = Integer.parseInt(arr1[i]);
+            int n2 = Integer.parseInt(arr2[j]);
+
+            if (n1 <= n2) {
+                arr[k++] = arr1[i++];
+            } else {
+                arr[k++] = arr2[j++];
+            }
+        }
+
+        while (i < arr1.length) {
+            arr[k++] = arr1[i++];
+        }
+
+        while (j < arr2.length) {
+            arr[k++] = arr2[j++];
+        }
         return arr;
     }
 }
