c3c0ca760719afd2dcd97268def62f6495ed5e80
badgelatte <saucy5939@gmail.com>
2026-02-23 23:58:24 +0900

    feat: 멀티스레드 동기화 문제 풀이 구현
    멀티스레드 동기화 문제 풀이 구현

--- src/main/java/com/again/day0223/Job.java
+++ src/main/java/com/again/day0223/Job.java
@@ -1,17 +1,37 @@
 package com.again.day0223;
 
 public class Job {
+    int count = 1;
 
     public void firstJob(Runnable job) throws InterruptedException {
+        while (true) {
+            if (count == 1) {
+                job.run();
+                count++;
+                break;
+            }
+        }
 
     }
 
     public void secondJob(Runnable job) throws InterruptedException{
-
+        while (true) {
+            if (count == 2) {
+                job.run();
+                count++;
+                break;
+            }
+        }
     }
 
     public void thirdJob(Runnable job) throws InterruptedException{
-
+        while (true) {
+            if (count == 3) {
+                job.run();
+                count++;
+                break;
+            }
+        }
     }
 
 }
