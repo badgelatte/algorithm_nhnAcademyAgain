d836ae47dda94b20bba7e2ed0dbaea96cf448b58
badgelatte <saucy5939@gmail.com>
2026-02-24 17:06:03 +0900

    feat: 동시성 제어 문제 구현 완료
    동시성 제어 문제 구현 완료

--- src/main/java/com/again/day0224/OneTwo.java
+++ src/main/java/com/again/day0224/OneTwo.java
@@ -3,17 +3,41 @@ package com.again.day0224;
 import java.util.concurrent.Semaphore;
 
 public class OneTwo {
-    private final Semaphore s1 = new Semaphore(0);
-    private final Semaphore s2 = new Semaphore(1);
+    private final Semaphore s1 = new Semaphore(1);
+    private final Semaphore s2 = new Semaphore(0);
     private final int count;
+    int current = 0;
 
     public OneTwo(int count) {
         this.count = count;
     }
 
-    public void printOne(Runnable runnable) throws InterruptedException {
+    public synchronized void printOne(Runnable runnable) throws InterruptedException {
+        while (current < count) {
+            while (!s1.tryAcquire()) {
+                wait();
+            }
+
+            if (current >= count) {
+                break;
+            }
+
+            runnable.run();
+            s2.release();
+            notifyAll();
+        }
     }
 
-    public void printTwo(Runnable runnable) throws InterruptedException {
+    public synchronized void printTwo(Runnable runnable) throws InterruptedException {
+        while (current < count) {
+            while (!s2.tryAcquire()) {
+                wait();
+            }
+
+            runnable.run();
+            s1.release();
+            current++;
+            notifyAll();
+        }
     }
 }
