b5bd1e3f656ca45d7d944169d8fdbb105c54e27e
badgelatte <saucy5939@gmail.com>
2026-03-06 22:54:38 +0900

    fix: synchoronized 삭제, Thread.spleep() 이용
    BlockingQueue에서 대기 시간 설정 가능하다는 것을 보고 wait()보다 sleep()을 이용하는게 취지에 맞다고 생각하여 수정

--- src/main/java/com/again/day0306/BoundedBlockingQueue.java
+++ src/main/java/com/again/day0306/BoundedBlockingQueue.java
@@ -19,23 +19,21 @@ public class BoundedBlockingQueue {
     }
 
     // queue큐가 가득찼을때 항목을 추가하려는 스레드나, 큐가 비었을때 항목을 제거하려는 스레드는 대기 상태로 만든다.
-    public synchronized void enqueue(int element) throws InterruptedException {
+    public void enqueue(int element) throws InterruptedException {
         // TODO 2.queue에 element를 추가합니다.
         // queue가 가득차면 enqueue 메서드를 호출하는 Thread는 queue가 가득 차지 않을 때까지 대기 합니다.
         while(size() > capacity) {
-            wait();
+            Thread.sleep(1000);
         }
-        notifyAll();
         queue.add(element);
     }
 
     // queue가 비어있으면 요소를 꺼내려는 스레드는 큐에 요소가 추가될 때까지 대기한다.
-    public synchronized int dequeue() throws InterruptedException {
+    public int dequeue() throws InterruptedException {
         // TODO 3.queue에서 뒤쪽에(가장 먼저 들어온) 있는 요소들을 반환하고 제거 합니다.
         while(size() < 1) {
-            wait();
+            Thread.sleep(1000);
         }
-        notifyAll();
         return queue.poll();
     }
 
