c600f721870848cfa073e22490a64102a9194d49
badgelatte <saucy5939@gmail.com>
2026-03-07 23:06:50 +0900

    feat: 최대 부분 배열 문제 구현 완료
    최대 부분 배열 문제 구현 완료

--- src/main/java/com/again/day0307/Solution.java
+++ src/main/java/com/again/day0307/Solution.java
@@ -2,6 +2,28 @@ package com.again.day0307;
 
 public class Solution {
     public static long maxSubarraySum(int arr[]){
-        return 1;
+        int max = arr[0];
+        int addNums = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                addNums = plusNums(arr, i, j);
+                if (max < addNums) {
+                    max = addNums;
+                }
+            }
+        }
+
+        return max;
+    }
+    
+    public static int plusNums(int[] arr, int start, int end) {
+        int plusNums = 0;
+
+        for (int i = start; i <= end; i++) {
+            plusNums += arr[i];
+        }
+
+        return plusNums;
     }
 }
