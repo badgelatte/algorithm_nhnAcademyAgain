a9d9f826ae38476b47863810e266a8b7253d926e
badgelatte <saucy5939@gmail.com>
2026-03-08 18:20:20 +0900

    feat: 배열을 이용한 평균 계산 구현 완료
    배열을 이용한 평균 계산 구현 완료

--- src/main/java/com/again/day0308/basic/Solution.java
+++ src/main/java/com/again/day0308/basic/Solution.java
@@ -4,12 +4,24 @@ public class Solution {
 
     public static double averageReturnDouble(double arr[]){
         // TODO 1.arr[]의 average(평균값 계산하기) 소숫점 2자리 까지 반올림 하여 반환
-        return 0d;
+        double avgArr = 0d;
+
+        if (arr.length < 1) {
+            throw new IllegalArgumentException();
+        }
+
+        for (double v : arr) {
+            avgArr += v;
+        }
+
+        avgArr /= arr.length;
+
+        return Math.round(avgArr * 100D) / 100D;
     }
 
     public static String averageReturnString(double... arr){
         // TODO 2.arr[]의 average(평균값 계산하기) - 소숫점 2자리 까지 반올림 하여 반환
 
-        return "";
+        return String.format("%.2f", averageReturnDouble(arr));
     }
 }
--- src/main/java/com/again/day0308/general/MyStack.java
+++ src/main/java/com/again/day0308/general/MyStack.java
@@ -9,6 +9,10 @@ public class MyStack implements Iterable {
     private final int capacity;
 
     public MyStack(int capacity) {
+        if (capacity < 1) {
+            throw new IllegalArgumentException();
+        }
+
         top = -1;
         this.capacity = capacity;
         arr = new int[capacity];
@@ -16,16 +20,29 @@ public class MyStack implements Iterable {
 
     public void push(int data) {
         // TODO 1.stack에 data를 넣습니다.
+        arr[++top] = data;
     }
 
     public int pop() {
         // TODO 2.데이터를 반환하고 반환된 데이터는 stack에서 제거됩니다.
-        return 0;
+        return arr[top--];
     }
 
     @Override
     public Iterator iterator() {
         // TODO 3.Iterator를 구현합니다.
-        return null;
+        return new Iterator() {
+            int next = top;
+
+            @Override
+            public boolean hasNext() {
+                return next >= 0;
+            }
+
+            @Override
+            public Object next() {
+                return arr[next--];
+            }
+        };
     }
 }
