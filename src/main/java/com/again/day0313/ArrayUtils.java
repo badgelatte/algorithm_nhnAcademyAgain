f64a8f5701308034b4ec93cf08eacfa00dc0e6c5
badgelatte <saucy5939@gmail.com>
2026-03-13 09:01:20 +0900

    feat: User 클래스를 이용한 ArrayUtils 클래스 구현하는 문제
    User 클래스를 이용한 ArrayUtils 클래스 구현하는 문제

---
+++ src/main/java/com/again/day0313/ArrayUtils.java
@@ -0,0 +1,8 @@
+package com.again.day0313;
+
+public final class ArrayUtils {
+    public static boolean equals(Object[] objects1 , Object[] objects2 ){
+        // TODO 1. 두 배열을 배열을 비교하여 일치하면 true , 일치하지 않다면 false를 반환 합니다.
+        return true;
+    }
+}
---
+++ src/main/java/com/again/day0313/User.java
@@ -0,0 +1,11 @@
+package com.again.day0313;
+
+public class User {
+    private final String userId;
+    private final String userName;
+
+    public User(String userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+    }
+}
